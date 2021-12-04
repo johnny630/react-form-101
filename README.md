4bc3305cbb225049362ab56b599ac4161da244bb
johnny630 <bakolama@gmail.com>
2021-12-04 11:40:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-form-101
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-form-101)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SimpleInput from './components/SimpleInput';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
